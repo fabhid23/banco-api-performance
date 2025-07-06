d2f58db262b1d342bbbd02222bb763e3277aea69
fabhid23 <fab.takahara94@gmail.com>
2025-07-06 19:41:26 -0300

    Adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,10 +3,15 @@ import { sleep, check } from 'k6';
 
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  //vus: 10,
+  //duration: '30s',
   // Define the number of iterations for the test
   //iterations: 50,
+  stages: [
+    { duration: '5s', target: 10 },
+    { duration: '20s', target: 10 },
+    { duration: '5s', target: 0 },
+  ],
    thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(90)<3000', 'max<5000'], // 95% of requests should be below 200ms
