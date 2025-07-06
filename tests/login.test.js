7b5ac4dafc0d81e1b8e4bc2dee271ba3838688c0
fabhid23 <fab.takahara94@gmail.com>
2025-07-06 19:54:54 -0300

    adicionei fixtures para os dados de entrada do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,17 +1,18 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 
 export const options = {
   //vus: 10,
   //duration: '30s',
   // Define the number of iterations for the test
-  //iterations: 50,
-  stages: [
-    { duration: '5s', target: 10 },
-    { duration: '20s', target: 10 },
-    { duration: '5s', target: 0 },
-  ],
+  iterations: 1,
+  // stages: [
+  //   { duration: '5s', target: 10 },
+  //   { duration: '20s', target: 10 },
+  //   { duration: '5s', target: 0 },
+  // ],
    thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(90)<3000', 'max<5000'], // 95% of requests should be below 200ms
@@ -21,10 +22,11 @@ export const options = {
 // The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
 export default function () {
   const url = 'http://localhost:3000/login';
-  const payload = JSON.stringify({
-    username: 'julio.lima',
-    senha: '123456',
-  });
+  postLogin.username = "junior.lima"
+  //console.log(postLogin);
+  const payload = JSON.stringify(
+    postLogin
+  );
 
   const params = {
     headers: {
